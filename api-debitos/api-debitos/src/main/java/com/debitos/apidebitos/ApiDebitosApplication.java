941629f785eaa771c65f48591efa325e12c02d01
Jarbas Adriano <jarbasadriano@hotmail.com>
2024-01-25 21:00:54 -0300

    Commitando o projeto

---
+++ api-debitos/api-debitos/src/main/java/com/debitos/apidebitos/ApiDebitosApplication.java
@@ -0,0 +1,13 @@
+package com.debitos.apidebitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDebitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDebitosApplication.class, args);
+	}
+
+}
---
+++ api-debitos/api-debitos/src/main/java/com/debitos/apidebitos/repository/DebitoParcelaRepository.java
@@ -0,0 +1,9 @@
+package com.debitos.apidebitos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.debitos.apidebitos.models.DebitoParcela;
+
+public interface DebitoParcelaRepository extends JpaRepository<DebitoParcela, Long>{
+
+}
